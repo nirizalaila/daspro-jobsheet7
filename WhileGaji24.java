6bec9b9769910338c3229c76a4b902a261013680
nirizalaila <nirizalailaumi1@gmail.com>
2023-10-31 13:02:41 +0700

    percobaan 2

---
+++ WhileGaji24.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class WhileGaji24 {
+    public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int jumlahKaryawan, jumlahJamLebur;
+    double gajiLembur, totalGajiLembur;
+    gajiLembur = 0;
+    totalGajiLembur = 0;
+    String jabatan;
+    
+    System.out.print("Masukkan jumlah karyawan: ");
+    jumlahKaryawan = scan.nextInt();
+
+    int i = 0;
+
+    while (i < jumlahKaryawan) {
+        System.out.println("Pilihan jabatan - Direktur, Manajer, Karyawan");
+        System.out.print("Masukkan jabatan karyawan ke-" + (i+1) + ": ");
+        jabatan = scan.next();
+        System.out.print("Masukkan jumlah jam lembur: ");
+        jumlahJamLebur = scan.nextInt();
+        i++;
+
+        if (jabatan.equalsIgnoreCase("direktur")) {
+            continue;  
+        } else if (jabatan.equalsIgnoreCase("manajer")) {
+            gajiLembur = jumlahJamLebur * 100000;
+        } else if (jabatan.equalsIgnoreCase("karyawan")) {
+            gajiLembur = jumlahJamLebur * 75000;
+        }
+        totalGajiLembur += gajiLembur;
+
+        System.out.println("Total gaji lembur: " + totalGajiLembur);
+    }
+    }
+}
